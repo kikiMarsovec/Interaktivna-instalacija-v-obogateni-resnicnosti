03c7e758e96dd48f97e97fe0c7e55411b811d0a8
Kristjan Kostanjšek <kristjan.kostanjsek@gmail.com>
2023-02-04 15:55:40 +0100

    Vstop v Tunel in animacija

--- Assets/_MyStuff/Scripts/NastavitevTunela.cs
+++ Assets/_MyStuff/Scripts/NastavitevTunela.cs
@@ -1,15 +1,30 @@
+using Microsoft.MixedReality.Toolkit.Input;
 using Microsoft.MixedReality.Toolkit.UI;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class NastavitevTunela : MonoBehaviour
 {
+	// ko je true, je Z rotacija cevke zaklenjena
 	private bool zakleniRotacijoZ = false;
 
+	// ciljna pozicija, rotacija in velikost cevke
 	private Vector3 tunelPozicija;
 	private Quaternion tunelRotacija;
 	private Vector3 tunelVelikost;
+
+	// da preverimo ali je tunel nastavljen preden vanj vstopimo
+	private bool tunelNastavljen = false;
+
+	// spremenljivka za zacetek animacije cevke v tunel
+	private bool premakniVTunel = false;
+
+	// spremenljivke za animacijo cevke v tunel
+	private Vector3 trenutnaHitrostTranslacije = Vector3.zero;
+	private Vector3 trenutnaHitrostSkaliranja = Vector3.zero;
+	private float casRotacije = 0.0f;
 
     [SerializeField]
 	[Tooltip("Dodaj DialogSmall_192x96 prefab")]
@@ -34,6 +49,7 @@ public class NastavitevTunela : MonoBehaviour
 		}
 		// spet omogocimo rotacijo po Z osi
 		zakleniRotacijoZ = false;
+		tunelNastavljen = true;
 	}
 
 	private void Update() {
@@ -41,5 +57,42 @@ public class NastavitevTunela : MonoBehaviour
 		if (zakleniRotacijoZ) {
 			transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, 0);
 		}
+		// TODO TUDI TO NI NAJBOLJ OPTIMALNO
+		// Ideja: prestavi ta Update v nek drug script, ki ga aktiviras, takrat, ko ga potrebujes, potem pa ga takoj deaktiviras
+		if (premakniVTunel) {
+			// preverimo ali je cevka priblizno na pravi poziciji, velikosti in rotaciji
+			if (Vector3.Distance(transform.position, tunelPozicija) < 0.1f && Quaternion.Angle(transform.rotation, tunelRotacija) < 1 && Vector3.Distance(transform.localScale, tunelVelikost) < 0.1f) {
+				// ko cevka doseze pravilno pozicijo, velikost in rotacijo, prenehamo
+				premakniVTunel = false;
+				trenutnaHitrostTranslacije = Vector3.zero;
+				trenutnaHitrostSkaliranja = Vector3.zero;
+				casRotacije = 0;
+				// vklopimo nazaj izklopljene atome
+				gameObject.transform.GetChild(0).transform.Find("Hydrogen_mesh").gameObject.SetActive(true);
+			} else {
+				// cevko transliramo, skaliramo in rotiramo proti ciljni poziciji, velikosti in rotaciji
+				transform.position = Vector3.SmoothDamp(transform.position, tunelPozicija, ref trenutnaHitrostTranslacije, 1f);
+				transform.localScale = Vector3.SmoothDamp(transform.localScale, tunelVelikost, ref trenutnaHitrostSkaliranja, 1f);
+				transform.rotation = Quaternion.Slerp(transform.rotation, tunelRotacija, casRotacije);
+				casRotacije += Time.deltaTime * 0.1f;
+			}
+		}
+	}
+
+	public void VstopiVTunel() {
+		if (!tunelNastavljen) {
+			// TODO prikazi error uporabniku
+			Debug.Log("ERROR: Prej je treba nastaviti tunel");
+			return;
+		}
+		// izklopimo komponente: box collider, object manipulator in near interaction grabbable
+		GetComponent<BoxCollider>().enabled = false;
+		GetComponent<ObjectManipulator>().enabled = false;
+		GetComponent<NearInteractionGrabbable>().enabled = false;
+
+		premakniVTunel = true; // zacne premikati cevko v Update funkciji
+
+		// zaradi premikanja cevke izklopimo dolocene atome za optimizacijo
+		gameObject.transform.GetChild(0).transform.Find("Hydrogen_mesh").gameObject.SetActive(false);
 	}
 }
