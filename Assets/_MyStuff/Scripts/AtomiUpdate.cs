b8d1b8cebbbef5ae3fa9b36c2b92c43aa82d4d6c
kkostanjsek <82522072+kikiMarsovec@users.noreply.github.com>
2023-02-10 12:01:39 +0100

    Update funkcija premaknjena v nov script,
    Nov script (AtomiUpdate) se aktivira samo takrat, ko se potrebuje, nato pa se deaktivira (OPTIMIZACIJA)

---
+++ Assets/_MyStuff/Scripts/AtomiUpdate.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// V AtomiUpdate.cs sem prestavil Update funkcijo iz NastavitevTunela.cs (Za optimizacijo).
+// Skripto bom aktiviral le takrat, ko jo potrebujem, takoj zatem pa jo bom deaktiviral.
+
+public class AtomiUpdate : MonoBehaviour {
+
+	// ko je true, je Z rotacija cevke zaklenjena
+	private bool zakleniRotacijoZ = false;
+
+	// ciljna pozicija, rotacija in velikost cevke
+	private Vector3 tunelPozicija;
+	private Quaternion tunelRotacija;
+	private Vector3 tunelVelikost;
+
+	// spremenljivka za zacetek animacije cevke v tunel
+	private bool premakniVTunel = false;
+
+	// spremenljivke za animacijo cevke v tunel
+	private Vector3 trenutnaHitrostTranslacije = Vector3.zero;
+	private Vector3 trenutnaHitrostSkaliranja = Vector3.zero;
+	private float casRotacije = 0.0f;
+
+	public void nastaviZaklenjenostRotacijeZ(bool zakleni) {
+		zakleniRotacijoZ=zakleni;
+	}
+
+	public void zacniAnimacijoCevke(Vector3 ciljnaPozcijia, Quaternion ciljnaRotacija, Vector3 ciljnaVelikost) {
+		tunelPozicija = ciljnaPozcijia;
+		tunelRotacija = ciljnaRotacija;
+		tunelVelikost = ciljnaVelikost;
+		premakniVTunel = true;
+
+		// zaradi premikanja cevke izklopimo dolocene atome za optimizacijo
+		gameObject.transform.GetChild(0).transform.Find("Hydrogen_mesh").gameObject.SetActive(false);
+	}
+
+	private void Update() {
+		if (zakleniRotacijoZ) {
+			transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, 0);
+		}
+		if (premakniVTunel) {
+			// preverimo ali je cevka priblizno na pravi poziciji, velikosti in rotaciji
+			if (Vector3.Distance(transform.position, tunelPozicija) < 0.1f && Quaternion.Angle(transform.rotation, tunelRotacija) < 1 && Vector3.Distance(transform.localScale, tunelVelikost) < 0.1f) {
+				// ko cevka doseze pravilno pozicijo, velikost in rotacijo, prenehamo
+				premakniVTunel = false;
+				trenutnaHitrostTranslacije = Vector3.zero;
+				trenutnaHitrostSkaliranja = Vector3.zero;
+				casRotacije = 0;
+				// vklopimo nazaj izklopljene atome
+				gameObject.transform.GetChild(0).transform.Find("Hydrogen_mesh").gameObject.SetActive(true);
+				// izklopimo AtomiUpdate.cs, ker ga ne potrebujemo vec
+				this.enabled = false;
+			}
+			else {
+				// cevko transliramo, skaliramo in rotiramo proti ciljni poziciji, velikosti in rotaciji
+				transform.position = Vector3.SmoothDamp(transform.position, tunelPozicija, ref trenutnaHitrostTranslacije, 1f);
+				transform.localScale = Vector3.SmoothDamp(transform.localScale, tunelVelikost, ref trenutnaHitrostSkaliranja, 1f);
+				transform.rotation = Quaternion.Slerp(transform.rotation, tunelRotacija, casRotacije);
+				casRotacije += Time.deltaTime * 0.1f;
+			}
+		}
+	}
+}
--- Assets/_MyStuff/Scripts/NastavitevTunela.cs
+++ Assets/_MyStuff/Scripts/NastavitevTunela.cs
@@ -5,10 +5,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering;
 
-public class NastavitevTunela : MonoBehaviour
-{
-	// ko je true, je Z rotacija cevke zaklenjena
-	private bool zakleniRotacijoZ = false;
+public class NastavitevTunela : MonoBehaviour {
+	// AtomiUpdate.cs skript
+	AtomiUpdate atomiUpdate;
 
 	// ciljna pozicija, rotacija in velikost cevke
 	private Vector3 tunelPozicija;
@@ -18,23 +17,42 @@ public class NastavitevTunela : MonoBehaviour
 	// da preverimo ali je tunel nastavljen preden vanj vstopimo
 	private bool tunelNastavljen = false;
 
-	// spremenljivka za zacetek animacije cevke v tunel
-	private bool premakniVTunel = false;
-
-	// spremenljivke za animacijo cevke v tunel
-	private Vector3 trenutnaHitrostTranslacije = Vector3.zero;
-	private Vector3 trenutnaHitrostSkaliranja = Vector3.zero;
-	private float casRotacije = 0.0f;
+	// ce veckrat pritisnemo na gumb za nastavljanje tunela, moramo dobiti samo en Dialog
+	private bool tunelVNastavljanju = false;
 
     [SerializeField]
 	[Tooltip("Dodaj DialogSmall_192x96 prefab")]
 	private GameObject dialogPrefab;
-    public void NastaviTunel() {
+
+	private void Start() {
+		atomiUpdate = GetComponent<AtomiUpdate>();
+		// Takoj na zacetku deaktiviramo atomiUpdate, da se Update funkcija ne bo izvajala po nepotrebnem
+		atomiUpdate.enabled = false;
+	}
+
+	public void NastaviTunel() {
 		// TODO FAJN BI BLO ZASCITIT TO Z NEKIM PIN-OM
+
+		// Ce je tunel ze v nastavljanju, ne odpremo novega Dialog-a
+		if (tunelVNastavljanju) {
+			Debug.Log("ERROR: Tunel je ze v nastavljanju.");
+			return;
+		}
+		tunelVNastavljanju = true;
+
+
 		// ustvari se nov dialog
 		Dialog nastavljanjeTunelaDialog = Dialog.Open(dialogPrefab, DialogButtonType.Confirm | DialogButtonType.Cancel, "Nastavljanje tunela", "Nastavite tunel na željeno pozicijo, rotacijo in velikost in potrdite.", true);
+
+		// vklopimo komponente: box collider, object manipulator in near interaction grabbable (ce je slucajno cevka ze nastavljena v tunel)
+		GetComponent<BoxCollider>().enabled = true;
+		GetComponent<ObjectManipulator>().enabled = true;
+		GetComponent<NearInteractionGrabbable>().enabled = true;
+
 		// zaklenemo rotacijo nanocevke po Z osi
-		zakleniRotacijoZ = true;
+		atomiUpdate.enabled = true;
+		atomiUpdate.nastaviZaklenjenostRotacijeZ(true);
+
 		// ko se dialog zapre klicemo funkcijo dialogZaprt
 		if (nastavljanjeTunelaDialog != null)
 			nastavljanjeTunelaDialog.OnClosed += dialogZaprt;
@@ -48,35 +66,10 @@ public class NastavitevTunela : MonoBehaviour
 			tunelVelikost = transform.localScale;
 		}
 		// spet omogocimo rotacijo po Z osi
-		zakleniRotacijoZ = false;
+		atomiUpdate.nastaviZaklenjenostRotacijeZ(false);
+		atomiUpdate.enabled = false;
 		tunelNastavljen = true;
-	}
-
-	private void Update() {
-		// TODO ALI JE TO OPTIMALEN NACIN ZA ZAKLEPANJE ROTACIJE PO Z OSI
-		if (zakleniRotacijoZ) {
-			transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, 0);
-		}
-		// TODO TUDI TO NI NAJBOLJ OPTIMALNO
-		// Ideja: prestavi ta Update v nek drug script, ki ga aktiviras, takrat, ko ga potrebujes, potem pa ga takoj deaktiviras
-		if (premakniVTunel) {
-			// preverimo ali je cevka priblizno na pravi poziciji, velikosti in rotaciji
-			if (Vector3.Distance(transform.position, tunelPozicija) < 0.1f && Quaternion.Angle(transform.rotation, tunelRotacija) < 1 && Vector3.Distance(transform.localScale, tunelVelikost) < 0.1f) {
-				// ko cevka doseze pravilno pozicijo, velikost in rotacijo, prenehamo
-				premakniVTunel = false;
-				trenutnaHitrostTranslacije = Vector3.zero;
-				trenutnaHitrostSkaliranja = Vector3.zero;
-				casRotacije = 0;
-				// vklopimo nazaj izklopljene atome
-				gameObject.transform.GetChild(0).transform.Find("Hydrogen_mesh").gameObject.SetActive(true);
-			} else {
-				// cevko transliramo, skaliramo in rotiramo proti ciljni poziciji, velikosti in rotaciji
-				transform.position = Vector3.SmoothDamp(transform.position, tunelPozicija, ref trenutnaHitrostTranslacije, 1f);
-				transform.localScale = Vector3.SmoothDamp(transform.localScale, tunelVelikost, ref trenutnaHitrostSkaliranja, 1f);
-				transform.rotation = Quaternion.Slerp(transform.rotation, tunelRotacija, casRotacije);
-				casRotacije += Time.deltaTime * 0.1f;
-			}
-		}
+		tunelVNastavljanju = false;
 	}
 
 	public void VstopiVTunel() {
@@ -90,9 +83,9 @@ public class NastavitevTunela : MonoBehaviour
 		GetComponent<ObjectManipulator>().enabled = false;
 		GetComponent<NearInteractionGrabbable>().enabled = false;
 
-		premakniVTunel = true; // zacne premikati cevko v Update funkciji
 
-		// zaradi premikanja cevke izklopimo dolocene atome za optimizacijo
-		gameObject.transform.GetChild(0).transform.Find("Hydrogen_mesh").gameObject.SetActive(false);
+		// Klicemo funkcioj, ki zacne premikati cevko v Update funkciji
+		atomiUpdate.enabled = true;
+		atomiUpdate.zacniAnimacijoCevke(tunelPozicija, tunelRotacija, tunelVelikost);
 	}
 }
